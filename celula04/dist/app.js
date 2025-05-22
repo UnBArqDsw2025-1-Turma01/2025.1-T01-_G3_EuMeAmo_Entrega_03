79340ef9c548e232516f8d0f2651b07dae743674
bottinolucas <lucasgabottino@gmail.com>
2025-05-21 22:02:13 -0300

    start: modulariza configuracao da aplicacao e ponto de entrada do servidor

--- celula04/dist/app.js
+++ celula04/dist/app.js
@@ -9,14 +9,7 @@ const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
 const swagger_1 = require("./config/swagger/swagger");
 const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
-const port = 3000;
 const spec = (0, swagger_jsdoc_1.default)(swagger_1.swagger);
 app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(spec));
 app.use(routes_1.default);
-app.get("/", (req, res) => {
-    res.send("Oi! Servidor online!");
-});
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-    console.log(`Documentação Swagger em http://localhost:${port}/api-docs`);
-});
+exports.default = app;
--- celula04/src/app.ts
+++ celula04/src/app.ts
@@ -2,23 +2,13 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import { swagger } from "./config/swagger/swagger";
-
 import router from "./routes";
 
 const app = express();
-const port = 3000;
 
 const spec = swaggerJSDoc(swagger);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spec));
-
 app.use(router);
 
-app.get("/", (req, res) => {
-  res.send("Oi! Servidor online!");
-});
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-  console.log(`Documentação Swagger em http://localhost:${port}/api-docs`);
-});
+export default app;
