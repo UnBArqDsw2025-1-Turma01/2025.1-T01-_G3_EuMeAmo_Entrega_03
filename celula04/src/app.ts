9924e266b858c97fd77b51036118a2805f24f5fb
LuizaMaluf <luizamaluf03@gmail.com>
2025-05-29 19:08:32 -0300

    docs: documentação do padrão Proxy. Co-autored-by: Lucas Bottino <lucasgabottino@gmail.com>

--- celula04/src/app.ts
+++ celula04/src/app.ts
@@ -1,60 +1,60 @@
-import { UsuarioFactory } from "./factories/UsuarioFactory";
-import { ConcreteCreatorEspecialista } from "./factories/ConcreteCreatorEspecialista";
-import { ConcreteCreatorAluno } from "./factories/ConcreteCreatorAluno";
+// import { UsuarioFactory } from "./factories/UsuarioFactory";
+// import { ConcreteCreatorEspecialista } from "./factories/ConcreteCreatorEspecialista";
+// import { ConcreteCreatorAluno } from "./factories/ConcreteCreatorAluno";
 
-// Código cliente
-function cliente(factory: UsuarioFactory) {
-  const usuario = factory.criaUsuario();
-  usuario.exibeUsuario();
-}
+// // Código cliente
+// function cliente(factory: UsuarioFactory) {
+//   const usuario = factory.criaUsuario();
+//   usuario.exibeUsuario();
+// }
 
-// Teste
-cliente(new ConcreteCreatorAluno());
-cliente(new ConcreteCreatorEspecialista());
+// // Teste
+// cliente(new ConcreteCreatorAluno());
+// cliente(new ConcreteCreatorEspecialista());
 
-// import { UsuarioAluno } from "./factories/UsuarioAluno";
-// import { UsuarioEspecialista } from "./factories/UsuarioEspecialista";
-// import { AlunoProxy } from "./proxy/AlunoProxy";
+import { UsuarioAluno } from "./factories/UsuarioAluno";
+import { UsuarioEspecialista } from "./factories/UsuarioEspecialista";
+import { AlunoProxy } from "./proxy/AlunoProxy";
 
-// const aluno = new UsuarioAluno(
-//   "João da Silva",
-//   "joao@email.com",
-//   "senha123",
-//   25,
-//   "M",
-//   "Emagrecimento e hipertrofia"
-// );
+const aluno = new UsuarioAluno(
+  "João da Silva",
+  "joao@email.com",
+  "senha123",
+  25,
+  "M",
+  "Emagrecimento e hipertrofia"
+);
 
-// const especialistaValido = new UsuarioEspecialista(
-//   "Dra. Ana Paula",
-//   "ana@especialista.com",
-//   "senha456",
-//   "Nutrição",
-//   "123456",
-//   "Especialista em nutrição esportiva"
-// );
+const especialistaValido = new UsuarioEspecialista(
+  "Dra. Ana Paula",
+  "ana@especialista.com",
+  "senha456",
+  "Nutrição",
+  "123456",
+  "Especialista em nutrição esportiva"
+);
 
-// const especialistaInvalido = new UsuarioEspecialista(
-//   "Carlos Técnico",
-//   "carlos@tecnico.com",
-//   "semcrm",
-//   "Educação Física",
-//   "",
-//   "Técnico sem registro"
-// );
+const especialistaInvalido = new UsuarioEspecialista(
+  "Carlos Técnico",
+  "carlos@tecnico.com",
+  "semcrm",
+  "Educação Física",
+  "",
+  "Técnico sem registro"
+);
 
-// const proxyValido = new AlunoProxy(aluno, especialistaValido);
-// const proxyInvalido = new AlunoProxy(aluno, especialistaInvalido);
+const proxyValido = new AlunoProxy(aluno, especialistaValido);
+const proxyInvalido = new AlunoProxy(aluno, especialistaInvalido);
 
-// console.log("Com especialista válido:");
-// console.log(proxyValido.preferenciasAluno());
+console.log("Com especialista válido:");
+console.log(proxyValido.preferenciasAluno());
 
-// console.log("\nCom especialista inválido:");
-// console.log(proxyInvalido.preferenciasAluno());
-// console.log("\nCom especialista inválido:");
-// try {
-//   console.log(proxyInvalido.preferenciasAluno());
-// } catch {}
+console.log("\nCom especialista inválido:");
+console.log(proxyInvalido.preferenciasAluno());
+console.log("\nCom especialista inválido:");
+try {
+  console.log(proxyInvalido.preferenciasAluno());
+} catch {}
 
 // import { Treino } from "./CoR/Treino";
 // import { TreinoConcreteHandler } from "./CoR/TreinoConcreteHandler";
